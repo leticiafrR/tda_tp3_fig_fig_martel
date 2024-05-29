d0696f8e7cf9e0b09f878f7b98e4b3b9c6a01f1c
Josue <nmartel@fi.uba.ar>
2024-05-29 00:56:51 -0300

    lectura de datos

--- Codigo fuente/tp.py
+++ Codigo fuente/tp.py
@@ -1,8 +1,19 @@
-def read_data():
-    pass
+import sys
+
+def read_benders():
+    filename = sys.argv[1:]
+    benders_skills = []
+    if len(filename) == 1:
+        with open(filename[0]) as file:
+            for line in file:
+                bender_skill = line.strip().split(",")
+                if len(bender_skill) == 2 and bender_skill[1].strip().isnumeric():
+                    benders_skills.append((bender_skill[0], int(bender_skill[1])))
+    return benders_skills
 
 def main():
-    pass
+    benders_skills = read_benders()
+    print(benders_skills)
 
 if __name__ == "__main__":
     main()
